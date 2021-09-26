8bd4d1e3807e7aceee130ecdc19de0734cd973c2
Ritik Tiwari <ritiktiwariwork@gmail.com>
2021-09-26 11:17:06 +0530

    Site hosting and Database linking

--- partials/_dbconnect.php
+++ partials/_dbconnect.php
@@ -1,19 +1,27 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "crms";
+// $server = "localhost";
+// $user = "root";
+// $password = "";
+// $database = "crms";
 
 
-$conn = mysqli_connect($server, $user, $password, $database);
+// $conn = mysqli_connect($server, $user, $password, $database);
 
-if(!$conn){
-    die("Not connect with server");
-    // echo '<script>alert("Not Successfull");</script>';
-}
-// else{
-//     echo '<script>alert("Successfull");</script>';
+// if(!$conn){
+//     die("Not connect with server");
 // }
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
